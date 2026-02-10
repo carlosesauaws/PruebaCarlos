7b21e48dadb32ea6ddd820e1ef9e120e00ffb5fa
carlosesauaws <carlosesau.aws@gmail.com>
2026-02-09 19:27:01 -0600

    Cambio inicial para uso de biblioteca
    Cambio inicial para uso de biblioteca

---
+++ .venv/Scripts/tests/test_1-LoginLogout_refactorObj.py
@@ -0,0 +1,28 @@
+   
+    #-------------------------------------------------------------------------------
+from playwright.sync_api import sync_playwright
+from playwright.sync_api import expect
+from bibliotecaObjetos.loginPage import LoginPage
+import pytest_playwright
+import pytest
+
+@pytest.mark.biblioteca
+def test_login_exitoso():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        # Instanciamos nuestra biblioteca/página
+        login_pg = LoginPage(page)
+        
+        # Llamamos a los elementos o métodos
+        login_pg.navigate()
+        login_pg.objCapturaCodigo.fill("UXTY789@!!1")
+        login_pg.objBotonLogin.click()
+        #page.wait_for_timeout(10000)
+
+        # Aserción de login
+        expect(login_pg.page).to_have_url("https://candidates-qa.contalink.com/")
+        
+        browser.close()
+            
---
+++ .venv/bibliotecaObjetos/loginPage.py
@@ -0,0 +1,18 @@
+###############################################################################################################
+from playwright.sync_api import Page
+
+class LoginPage:
+    def __init__(self, page: Page):
+        self.page = page
+        # --- Biblioteca de Elementos (Locators) ---
+        self.objCapturaCodigo = page.locator("//*[@id='access-code']")
+        self.objBotonLogin = page.get_by_text("Validar Código")
+        self.objBotonLogout = page.get_by_role("button", name="Cerrar Sesión")
+
+    # --- Acciones (Métodos) ---
+    def navigate(self):
+        self.page.goto("https://candidates-qa.contalink.com/")
+
+    #def login(self, user, password):
+    #    self.objCapturaCodigo.fill("UXTY789@!!1")
+    #    self.objBotonLogin.click()
